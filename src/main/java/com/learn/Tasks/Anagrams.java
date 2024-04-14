340e71cf3c99556f00695bb425d27aa784b84b1b
Shalini Shanigarapu <shalini.shanigarapu99@gmail.com>
2024-04-14 17:24:02 +0530

    Anagrams

---
+++ src/main/java/com/learn/Tasks/Anagrams.java
@@ -0,0 +1,29 @@
+package com.learn.Tasks;
+
+
+
+import java.util.Arrays;
+
+public class Anagrams {
+    public static void main(String[] args) {
+        String str1="Heart";
+        String str2="earth";
+        if (anagram(str1,str2)){
+            System.out.println(str1+" and "+str2+" are Anagrams");
+        }else{
+            System.out.println(str1+" and "+str2+" are not Anagrams");
+        }
+    }
+    public static boolean anagram(String str1,String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        } else {
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+            return Arrays.equals(charArray1, charArray2);
+
+        }
+    }
+}
